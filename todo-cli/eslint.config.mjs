ada17a32c6adf9db19c8cf2f80ec27dfb13a0302
itrakesh2004 <rrveerit2004@gmail.com>
2024-09-05 16:27:09 +0530

    fix: Fix file name

---
+++ todo-cli/eslint.config.mjs
@@ -0,0 +1,15 @@
+// .mjs file content
+export default {
+    env: {
+      browser: true,
+      commonjs: true,
+      es2021: true,
+    },
+    extends: "eslint:recommended",
+    overrides: [],
+    parserOptions: {
+      ecmaVersion: "latest",
+    },
+    rules: {}
+  };
+  
